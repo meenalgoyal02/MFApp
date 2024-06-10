892edd69396ed83412b52448f1ee677d784847b3
meenalgoyal02 <67640357+meenalgoyal02@users.noreply.github.com>
2024-06-10 18:05:22 +0530

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,11 +1,2 @@
 
-CREATE TABLE students (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  age INTEGER
-);
-
-INSERT INTO students (name, age) VALUES
-  ('Meenal', 40),
-  ('Ali', 28),
-  ('Teoh', 18);
+alter table students add column phoneNum varchar(10);
