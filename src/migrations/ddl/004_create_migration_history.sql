3c0a62cb425cab55880b16a5bca55b6de53e1b18
Meenal-Goyal <meenal.goyal@coxautoinc.com>
2024-06-20 05:18:56 +0530

    SQL script committed.

--- src/migrations/ddl/004_create_migration_history.sql
+++ src/migrations/ddl/004_create_migration_history.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS "__MigrationsHistory" (
-    migration_id character varying(150) NOT NULL,
+CREATE TABLE IF NOT EXISTS "Migration_History" (
+    id SERIAL PRIMARY KEY,
     product_version character varying(32) NOT NULL,
     CONSTRAINT pk___ef_migrations_history PRIMARY KEY (migration_id)
 );
 
-insert into __MigrationsHistory("4");
+insert into Migration_History(product_version) values('4');
