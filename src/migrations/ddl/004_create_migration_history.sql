75911711dcafac7d8d14f731e7e28d9f5eaf954c
Meenal-Goyal <meenal.goyal@coxautoinc.com>
2024-06-20 05:13:33 +0530

    d

---
+++ src/migrations/ddl/004_create_migration_history.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "__MigrationsHistory" (
+    migration_id character varying(150) NOT NULL,
+    product_version character varying(32) NOT NULL,
+    CONSTRAINT pk___ef_migrations_history PRIMARY KEY (migration_id)
+);
+
+insert into __MigrationsHistory("4");
