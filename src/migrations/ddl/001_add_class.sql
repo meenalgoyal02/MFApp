7ed0adccdf7db4f0a2c0d150b4bcc0b972403c72
Meenal-Goyal <meenal.goyal@coxautoinc.com>
2024-06-19 17:07:20 +0530

    updated script definition.

--- src/migrations/ddl/001_add_class.sql
+++ src/migrations/ddl/001_add_class.sql
@@ -1,3 +1,14 @@
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER
+);
+
+INSERT INTO students (name, age) VALUES
+  ('Meenal', 40),
+  ('Ali', 28),
+  ('Teoh', 18);
+
 
 INSERT INTO students (name, age) VALUES
   ('Deepak', 40),
