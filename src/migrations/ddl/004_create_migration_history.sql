48d8b121120eab7d66127cf15cd95f8b16049a54
Meenal-Goyal <meenal.goyal@coxautoinc.com>
2024-06-20 06:08:07 +0530

    test script.

--- src/migrations/ddl/004_create_migration_history.sql
+++ src/migrations/ddl/004_create_migration_history.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS "Migration_History" (
     id SERIAL PRIMARY KEY,
-    product_version character varying(32) NOT NULL,
-    CONSTRAINT pk___ef_migrations_history PRIMARY KEY (migration_id)
+    product_version character varying(32) NOT NULL
 );
 
 insert into Migration_History(product_version) values('4');
