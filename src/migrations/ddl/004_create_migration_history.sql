5c2cc9a895ec8c17cb274ebcb904d048a8950786
Meenal-Goyal <meenal.goyal@coxautoinc.com>
2024-06-20 18:59:27 +0530

    updated script.

--- src/migrations/ddl/004_create_migration_history.sql
+++ src/migrations/ddl/004_create_migration_history.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS "__MigrationsHistory" (
     product_version character varying(32) NOT NULL
 );
 
-insert into "__MigrationsHistory"(product_version) values('4');
+insert into "__MigrationsHistory"(migration_id,product_version) values(2,'4');
